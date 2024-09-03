602b99140ead467cb876dfc963e58c75bee9ec11
Max Wallin <modestotechnology@gmail.com>
2024-09-03 11:40:50 +0200

    Improved logic for setting theme

--- src/js/theme-toggle.js
+++ src/js/theme-toggle.js
@@ -1,26 +1,40 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleCheckbox = document.getElementById('theme-toggle');
-    const themeToggleContainer = document.getElementById('theme-toggle-container');
+const themeToggle = document.getElementById("theme-toggle");
 
-    const savedTheme = localStorage.getItem('theme');
+document.addEventListener("DOMContentLoaded", () => {
+  const themeToggleContainer = document.getElementById(
+    "theme-toggle-container"
+  );
 
-    if (savedTheme) {
-        document.documentElement.setAttribute('data-theme', savedTheme);
-        toggleCheckbox.checked = savedTheme === 'dark';
-    } else {
-        // Get the current theme from the document or default to light
-        const currentTheme = document.documentElement.getAttribute('data-theme') || 'light';
-        toggleCheckbox.checked = currentTheme === 'dark';
-    }
+  const savedTheme = localStorage.getItem("theme");
 
-    // Remove the hidden class to display the toggle and logo
-    themeToggleContainer.classList.remove('hidden');
+  var theme;
 
-    // Update theme based on checkbox state
-    toggleCheckbox.addEventListener('change', () => {
-      const newTheme = toggleCheckbox.checked ? 'dark' : 'light';
-      document.documentElement.setAttribute('data-theme', newTheme);
-      // Save the theme preference in localStorage
-      localStorage.setItem('theme', newTheme);
-    });
-  });
+  if (savedTheme) {
+    document.documentElement.setAttribute("data-theme", savedTheme);
+    theme = savedTheme;
+  } else {
+    // Get the current theme from the document or default to light
+    const currentTheme =
+      document.documentElement.getAttribute("data-theme") || "light";
+    theme = currentTheme;
+  }
+
+  updateLogo(theme);
+  themeToggle.checked = theme === "dark";
+
+  // Remove the hidden class to display the toggle and logo
+  themeToggleContainer.classList.remove("hidden");
+});
+
+themeToggle.addEventListener("change", function () {
+  const newTheme = themeToggle.checked ? "dark" : "light";
+  document.documentElement.setAttribute("data-theme", newTheme);
+  updateLogo(newTheme);
+  localStorage.setItem("theme", newTheme);
+});
+
+function updateLogo(theme) {
+  const logo = document.getElementById("navbar-logo");
+  var logoResource = theme === 'dark' ? "/images/logo_light.png" : "/images/logo_dark.png";
+  logo.setAttribute("src", logoResource);
+}
