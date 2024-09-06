27ec1e08cc3d3e7caf00bda8a3e39d7771dc59c7
Max Wallin <modestotechnology@gmail.com>
2024-09-06 09:33:47 +0200

    Improved resume

--- src/js/printButton.js
+++ src/js/printButton.js
@@ -1,5 +1,10 @@
 const printButton = document.getElementById("print-button");
 
 printButton.addEventListener("click", function() {
+    var previousTitle = document.title;
+    var now = new Date();
+    var formattedDate = now.toISOString().split('T')[0]
+    document.title = `Max Modesto Wallin - Resume (printed ${formattedDate})`;
     window.print();
+    document.title = previousTitle;
 });
