7f93b74219ea5e0c7903adc1b9ce50d55e30d212
Max Wallin <modestotechnology@gmail.com>
2024-09-09 09:51:28 +0200

    Implemented contact on resume and feature toggle for description, and some styling

---
+++ src/js/contactInfo.js
@@ -0,0 +1,6 @@
+export const contactInfo = {
+  email: "modestotechnology@gmail.com",
+  phone: "+46705631571",
+  linkedinHandle: "maxmw",
+  githubHandle: "modestotech",
+};
--- src/js/populateContactPage.js
+++ src/js/populateContactPage.js
@@ -1,29 +1,36 @@
+import { contactInfo } from './contactInfo.js'
+
 document.addEventListener('DOMContentLoaded', function() {
   // Email
+  var email = contactInfo.email;
   const emailElement = document.createElement('a');
-  emailElement.href = 'mailto:modestotechnology@gmail.com';
-  emailElement.textContent = 'modestotechnology@gmail.com';
+  emailElement.href = `mailto:${email}`;
+  emailElement.textContent = email;
 
   // Phone
+  var phone = contactInfo.phone;
   const phoneElement = document.createElement('a');
-  phoneElement.href = 'tel:+46705631571';
-  phoneElement.textContent = '+46705631571';
+  phoneElement.href = `tel:${phone}`;
+  phoneElement.textContent = phone;
 
   // LinkedIn
+  var linkedInHandle = contactInfo.linkedinHandle;
   const linkedinElement = document.createElement('a');
-  linkedinElement.href = 'https://www.linkedin.com/in/maxmw';
-  linkedinElement.textContent = 'linkedin.com/in/maxmw';
+  linkedinElement.href = `https://www.linkedin.com/in/${linkedInHandle}`;
+  linkedinElement.textContent = `linkedin.com/in/${linkedInHandle}`;
   linkedinElement.target = '_blank';
 
   // Github
+  var githubHandle = contactInfo.githubHandle;
   const githubElement = document.createElement('a');
-  githubElement.href = 'https://github.com/modestotech';
-  githubElement.textContent = 'github.com/modestotech';
+  githubElement.href = `https://github.com/${githubHandle}`;
+  githubElement.textContent = `github.com/${githubHandle}`;
   githubElement.target = '_blank';
 
   // Insert content into the HTML
-  document.querySelector('.card-body .space-y-4').children[0].appendChild(emailElement);
-  document.querySelector('.card-body .space-y-4').children[1].appendChild(phoneElement);
-  document.querySelector('.card-body .space-y-4').children[2].appendChild(linkedinElement);
-  document.querySelector('.card-body .space-y-4').children[3].appendChild(githubElement);
+  const contactContainer = document.querySelector('#contact-container');
+  contactContainer.children[0].appendChild(emailElement);
+  contactContainer.children[1].appendChild(phoneElement);
+  contactContainer.children[2].appendChild(linkedinElement);
+  contactContainer.children[3].appendChild(githubElement);
 });
